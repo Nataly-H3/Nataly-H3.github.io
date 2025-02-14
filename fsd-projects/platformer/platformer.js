781e1210244cdbe0b70d0137e1e9872c94644bd9
Nataly-H3 <nataly.hernandez@jpschools.org>
2025-02-14 16:35:29 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,23 +27,31 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(500, 680, 300, 20, "blue");
+    createPlatform(900, 550, 200, 20, "blue");
+    createPlatform(450, 500, 300, 20, "blue");
+    createPlatform(100, 400, 200, 20, "blue");
+    createPlatform(500, 300, 100, 20, "blue");
+    createPlatform(800, 250, 200, 20, "blue");
+    createPlatform(1150, 200, 150, 20, "blue");
+    createPlatform(100, 100, 20, 300, "blue");
 
     // TODO 3 - Create Collectables
-
+    createCollectable("diamond", 200, 170, 0.3);
+    createCollectable("steve", 900, 200);
+    createCollectable("kennedi", 500, 550, 0.1);
 
 
     
     // TODO 4 - Create Cannons
-
-
-    
+    createCannon("top", 400, 1000);
+    createCannon("bottom", 1050, 850);
+    createCannon("right", 330, 2000);
+    createCannon("left", 505, 850);
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
